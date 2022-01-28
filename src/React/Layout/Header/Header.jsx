9b0749516cdf75a47a105fad75cc6837e330d864
Claire Zhao <eclairsclaire@gmail.com>
2022-01-27 20:39:14 -0800

    Styling Main Menu

--- src/React/Layout/Header/Header.jsx
+++ src/React/Layout/Header/Header.jsx
@@ -30,16 +30,10 @@ const HeaderStyled = styled.header`
         align-items: center;
 
         .Logo {
-            outline: solid 1px white;
             flex: 1;
             padding-right: 20px;
             display: flex;
             align-items: center;
-        }
-
-        .SiteTitle {
-            outline: solid 1px white;
-            flex: 3;
         }
     }
 `;
