97d61504a1a1ec3d2f685dcdc8200cce4f289405
Claire Zhao <eclairsclaire@gmail.com>
2022-02-03 18:13:21 -0800

    Updated Styles Media Query

--- src/React/Layout/Nav/MainMenu.jsx
+++ src/React/Layout/Nav/MainMenu.jsx
@@ -3,16 +3,12 @@ import styled, { css } from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 /* Scripts ---------------------------*/
-import { useMediaQuery } from 'React/common/useMediaQuery.js';
+import { defaultMediaQueries } from 'React/common/useMediaQuery.js';
 
 const MainMenu = () => {
 
-    const { media } = useMediaQuery();
-
-    console.log('media', media);
-
     return (
-        <MainMenuStyled className='MainMenu' media={ media }>
+        <MainMenuStyled className='MainMenu' >
             <NavLink to={ '/' } exact>Welcome</NavLink>
             <NavLink to={ '/services' }>Services</NavLink>
             <NavLink to={ '/contact' }>Contact</NavLink>
@@ -49,10 +45,7 @@ const MainMenuStyled = styled.div`
 
     }
 
-    ${
-        ({media}) => {
-            if (media.mdUp) {
-                return css`
+    @media ${defaultMediaQueries.mdUp} {
                     display: flex;
                     justify-content: center;
                     a {
@@ -62,8 +55,6 @@ const MainMenuStyled = styled.div`
                         font-size: 16px;
                         border-radius: 5px;
                     }
-                `;
-            }
         }
     }
 `;
