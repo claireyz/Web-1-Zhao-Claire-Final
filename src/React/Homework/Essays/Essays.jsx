a2ab2209136b7d339d9493b7eb80dac57c09c1bc
Claire Zhao <eclairsclaire@gmail.com>
2022-02-03 21:07:39 -0800

    Added Essay 7 Answer

--- src/React/Homework/Essays/Essays.jsx
+++ src/React/Homework/Essays/Essays.jsx
@@ -48,10 +48,10 @@ const Essays = () => {
 
             <Essay q='S7.  What is the difference between jpg, gif, png and SVG images?' >
                 <ul>
-                    <li><b>JPG:</b> </li>
-                    <li><b>GIF:</b> </li>
-                    <li><b>PNG:</b> </li>
-                    <li><b>SVG:</b> </li>
+                    <li><b>JPG:</b>JPGs are optimized for photographs because they are lossy raster files. This means they lose a small amount of information every time the JPG is saved. However, reducing the file size also results in reducing the image quality.  JPGs compress color so they are suitable for photographs and print.  </li>
+                    <li><b>GIF:</b>GIFs are in a bitmap format which supports up to 8 bits per pixel. This limits its color palette to 256px which is not optimized for photographs. GIFs are used most commonly in animated memes and not good for designs.  </li>
+                    <li><b>PNG:</b> PNGs are raster graphics that are designed to use on the web. It is the most largely used image compression format on the internet and is not optimized for printing professional graphic images. PNGs are good with line art images, icons, and graphics but can result in large file sizes. PNGs also have alpha transparency which means that each pixel has its own opacity level. PNGs come in 2 types: PNG-8 (the file has only 8 bits/pixel) and PNG-24 (24 bits/pixel). </li>
+                    <li><b>SVG:</b>SVGs are vector graphics that support animation. Their behaviors are defined in XML files for they can be searched, indexed, scripted, and compressed. SVGs are food for logos and line art because they’re not made from pixels and can be resized without losing quality. SVGs are the best in creating icons, line art, and high resolution art. </li>
                 </ul>
             </Essay>
 
