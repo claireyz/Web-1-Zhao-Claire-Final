a0862e9c942efc6443aced6192cf7ff26819a196
Claire Zhao <eclairsclaire@gmail.com>
2022-02-07 20:34:39 -0800

    Fleshing out services

--- src/React/Services/ServiceMenu/ServiceMenu.jsx
+++ src/React/Services/ServiceMenu/ServiceMenu.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ServiceMenuItem from './ServiceMenuItem.jsx';
-const ServiceMenu = () => {
+
+const ServiceMenu = ({categories}) => {
 
     return (
         <ServiceMenuStyled className='ServiceMenu'>
-            ServiceMenu 
-            <ServiceMenuItem />
+            {
+                categories.map((category, idx) => { 
+                return <ServiceMenuItem key={ idx } category={ category }/>
+                })
+            }
         </ServiceMenuStyled>
     );
 }
@@ -15,5 +19,7 @@ const ServiceMenu = () => {
 export default ServiceMenu;
 
 const ServiceMenuStyled = styled.div`
-    
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
 `;
