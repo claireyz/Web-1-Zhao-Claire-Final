1712a5d9a6672230a1cce7cbd5baf63e0a180ba7
Claire Zhao <eclairsclaire@gmail.com>
2022-03-07 19:11:50 -0800

    Slideshow: Implemented Carousel Library

--- src/React/Welcome/Slideshow/Slideshow.jsx
+++ src/React/Welcome/Slideshow/Slideshow.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { slidesData } from './slidesData.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
+import { slidesData } from './slidesData.js';
 import Slide from './Slide.jsx';
 
 const Slideshow = () => {
     return (
         <SlideshowStyled className='Slideshow'>
-            Slideshow 
-
-            {
-                slidesData.map((slide, idx) => {
-                    return <Slide key={ idx } slide={ slide } />
-                })
-            }
+            <Carousel
+                autoPlay
+                infiniteLoop
+            >
+                {
+                    slidesData.map((slide, idx) => {
+                        return <Slide key={ idx } slide={ slide } />
+                    })
+                }
+            </Carousel>
         </SlideshowStyled>
     );
 }
@@ -23,4 +28,8 @@ export default Slideshow;
 
 const SlideshowStyled = styled.div`
     
+
+    .carousel .slide .legend {
+        background-color: teal;
+    }
 `;
