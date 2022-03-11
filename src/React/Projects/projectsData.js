749d136c36584336869bcc34aa97114e85bb97ac
Claire Zhao <eclairsclaire@gmail.com>
2022-03-10 21:38:16 -0800

    updating projects images

--- src/React/Projects/projectsData.js
+++ src/React/Projects/projectsData.js
@@ -1,7 +1,8 @@
 export const projectsData =  {
-    categories: ['All', 'Massage', 'Water', 'Ammenities'],
+    categories: ['All', 'Healthcare', 'Finance', 'Wellbeing'],
     services: [
-        { id: 1, image: '/assets/img/Projects/the pie chart.jpg', title: 'Sauna', category: 'Ammenities', description: 'A description of our service...', cost: 9000 },
-        { id: 2, image: '/assets/img/Projects/empawered hero image.png', title: 'Swimming Lanes', category: 'Water', description: 'A description of our service...', cost: 15000 },
+        { id: 1, image: '/assets/img/Projects/thepiechart.jpg', title: 'The Pie Chart', category: 'Finance', description: 'A conceptual project where I design a financial goal tracking app to help foodies.' },
+        { id: 2, image: '/assets/img/Projects/empaweredheroimage.png', title: 'EmPawered', category: 'Healthcare', description: 'A conceptual project where I visualize and streamline the emergency appointment process for pet owners.' },
+        { id: 3, image: '/assets/img/Projects/anandaheroimage.png', title: 'Ananda.org', category: 'Wellbeing', description: 'A real world redesign of the Ananda.org user portal.' },
     ],
 }
